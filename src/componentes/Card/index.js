82c2d7372e08dd648298e09d44bb1ed41c8cc58e
henrique <henriquecordeiro054@gmail.com>
2023-05-11 17:56:33 -0300

    update filmes

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,29 +1,30 @@
-import styles from './Card.module.css';
-import iconFavoritar from './favoritar.png';
-import iconDesfavoritar from './desfavoritar.png';
 import { useFavoritoContext } from '../Contexto/favoritos';
+import styles from './Card.module.css';
+import iconeFavoritar from './favoritar.png';
+import iconeDesfavoritar from './desfavoritar.png';
 import { Link } from 'react-router-dom';
 
-export default function Card({ id, titulo, capa }) {
-    const {favorito,adicionarFavorito} = useFavoritoContext();
-    const ehfavorito = favorito.some((fav) =>fav.id ===id);
-    const icone = !ehfavorito ? iconFavoritar : iconDesfavoritar
+function Card({ id, titulo, capa }) {
+    
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
     return (
         <div className={styles.container}>
-            
-            <Link className={styles.Link} to={`/${id}`}>
+            <Link className={styles.link} to={`/${id}`}>
                 <img src={capa} alt={titulo} className={styles.capa} />
                 <h2>{titulo}</h2>
             </Link>
-         
+          
             <img src={icone}
                 alt="Favoritar filme"
-                className={styles.favoritar} 
-                onClick={()=>
-                {adicionarFavorito({ id, titulo, capa })}}/>
-                
+                className={styles.favoritar}
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }} />
         </div>
 
     )
 }
 
+export default Card;
