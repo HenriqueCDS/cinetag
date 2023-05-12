1cf1a9afee6a11d48e48423817b04497d9a01239
henrique <henriquecordeiro054@gmail.com>
2023-05-11 23:00:22 -0300

    update response

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -10,7 +10,7 @@ export default function Inicio() {
         <>
            
             <Banner imagem="home" />
-            <Titulo><h1>Um lugar para guardar seus videos e filmes</h1></Titulo>
+            <Titulo><h1>Um lugar que eu uso para guardar meus filmes </h1></Titulo>
             <section className={styles.container}>
                 {videos.map((video) => {
                     return <Card {...video} key={video.id} />
