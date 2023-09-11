e23d305a18a123d9bb1b07ffa0d316904e791bc3
henrique <henriquecordeiro054@gmail.com>
2023-09-10 23:13:45 -0300

    update

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,7 +3,7 @@ import styles from './Rodape.module.css';
 function Rodape() {
     return (
         <footer className={styles.rodape}>
-            <h2>Desenvolvido por Alura.</h2>
+            <h2>© Copyright 2022. Made by Henrique</h2>
         </footer>
     )
 }
